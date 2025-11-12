6d6615f034e874dbf41fe7a65caa11566ecbb91c
csAyushDubey <ayush.dubey@contentstack.com>
2025-11-12 16:10:57 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // Enable CORS
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
   
   const port = process.env.PORT || 3000;
   await app.listen(port);
